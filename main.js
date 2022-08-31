b329a809a66a3055d414f7dbfc7a5b3d60b85ec9
Érico Murta <66687324+ericomurta1991@users.noreply.github.com>
2022-08-31 12:05:30 -0300

    atualização main.js

--- main.js
+++ main.js
@@ -1,4 +1,57 @@
-document.querySelector('.tecla_pom');
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
+}
 
-document.querySelector('#som_tecla_pom').play();
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
+let contador = 0;
+
+//enquanto
+while (contador < listaDeTeclas.length) {
+    listaDeTeclas[contador].onclick = tocaSom;
+    contador = contador + 1;
+
+    console.log(contador);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//listaDeTeclas[0].onclick = tocaSomPom;
+
+
+
+/*
+function tocaSomPom() {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+
+function tocaSomPom(){
+    document.querySelector('#som_tecla_pom').play();
+}
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+
+function tocaSomClap(){
+    document.querySelector('#som_tecla_clap').play();
+}
+
+document.querySelector('.tecla_clap').onclick = tocaSomClap;
+
+
+ */
