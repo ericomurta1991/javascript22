19d8524ca250bfcad49000c1c58e95c857fd21cb
Érico Murta <66687324+ericomurta1991@users.noreply.github.com>
2022-09-11 18:03:16 -0300

    projeto finalizado

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-function tocaSom(idElementoAudio){
-    document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio){
+    const elemento = document.querySelector(seletorAudio).play();
+
+    if (elemento != null && elemento.localName === 'audio') {
+            elemento.play();
+    }
+    else {
+        //alert('Elemento não encontrado');
+        console.log('Elemento não encontrado ou seletor invalido');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -10,17 +18,35 @@ let contador = 0;
 while (contador < listaDeTeclas.length) {
 
     const tecla = listaDeTeclas[contador];
+
     const instrumento = tecla.classList[1];
 
-    console.log(instrumento)
+   //console.log(instrumento);
 
-    listaDeTeclas[contador].onclick = function (){
-        tocaSom('#som_tecla_pom');
+    //template string
+    const idAudio = `#som_${instrumento}`;
+    //console.log(idAudio);
+
+    tecla.onclick = function (){
+        tocaSom(idAudio);
     }
 
     contador = contador + 1;
 
     console.log(contador);
+
+
+    tecla.onkeydown = function (evento){
+
+        if (evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('ativa');
+        }
+
+    }
+
+    tecla.onkeyup = function (){
+        tecla.classList.remove('ativa');
+    }
 }
 
 
